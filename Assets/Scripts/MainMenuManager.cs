8d29819a725204b1fc0187af0c70105c76e97a65
Bruno Joaquim <bruno_dev@outlook.com>
2018-05-02 21:16:05 -0300

    Main menu and sprites

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour {
+
+    public string levelToLoad;
+
+	public void NewGame() {
+        SceneManager.LoadScene(levelToLoad);
+    }
+
+    public void QuitGame() {
+        Application.Quit(); 
+    }
+
+}
