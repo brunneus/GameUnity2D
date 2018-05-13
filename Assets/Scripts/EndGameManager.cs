6fdcf8eec0d63cf8438b7ecc64af9fc26150be94
Pati Guidini <pati_guidini@hotmail.com>
2018-05-12 23:26:54 -0300

    botão nenu principal; fonte montserrat

--- Assets/Scripts/EndGameManager.cs
+++ Assets/Scripts/EndGameManager.cs
@@ -2,18 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class EndGameManager : MonoBehaviour {
 
     public Text score;
 
-	// Use this for initialization
-	void Start () {
+    public void TryAgain()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    // Use this for initialization
+    void Start () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 }
