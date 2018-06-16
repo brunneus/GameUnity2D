13081f3d4f8e24f7fccf953bff92937064e6752e
Bruno Joaquim <bruno_dev@outlook.com>
2018-06-15 22:20:25 -0300

    camera Y

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,20 +7,28 @@ public class CameraController : MonoBehaviour {
 	public GameObject player;
 	public float endXPosition;
 
-    private float offset;        
+    private float offset;
+	private float originalCameraY;
 
     void Start()
     {
         offset = transform.position.x - 0.9f;
+		originalCameraY = transform.position.y;
     }
 
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
+		var cameraY = player.transform.position.y > 8.5
+			? originalCameraY + (player.transform.position.y - 8.5f)
+			: originalCameraY;
+		
 		if (player != null && !player.GetComponent<PlayerPlatformerController>().isLocked() && player.transform.position.x < endXPosition && player.transform.position.x > 0.14)
         transform.position = new Vector3(
 			player.transform.position.x + offset, 
-            transform.position.y, 
+			cameraY, 
             transform.position.z);
+		
+		Debug.Log (Time.deltaTime);
     }
 }
